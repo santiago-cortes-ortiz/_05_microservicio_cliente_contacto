8e807af7f02917de930e055ee1b07ec48bd89ea6
santiago cortes <jeissoncortes02@outlook.com>
2021-08-06 11:46:01 -0500

    manejo con response entity

--- src/main/java/inicio/controller/PersonaController.java
+++ src/main/java/inicio/controller/PersonaController.java
@@ -2,10 +2,14 @@ package inicio.controller;
 
 import inicio.model.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -22,13 +26,20 @@ public class PersonaController {
     String url = "http://localhost:8080";
 
     @GetMapping(value = "/personas/{nombre}/{email}/{edad}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Persona> altaPersona(@PathVariable("nombre") String nombre,
-                                     @PathVariable("email")String email,
-                                     @PathVariable("edad")int edad){
+    public ResponseEntity<List<Persona>> altaPersona(@PathVariable("nombre") String nombre,
+                                                    @PathVariable("email")String email,
+                                                    @PathVariable("edad")int edad){
         Persona persona = new Persona(nombre,email,edad);
-        restTemplate.postForLocation(url+"/contactos",persona);
-        Persona[] personas = restTemplate.getForObject(url+"/contactos",Persona[].class);
-        return Arrays.asList(personas);
+        try{
+            restTemplate.postForLocation(url+"/contactos",persona);
+            Persona[] personas = restTemplate.getForObject(url+"/contactos",Persona[].class);
+            return new ResponseEntity<>(Arrays.asList(personas), HttpStatus.CREATED);
+        }catch(HttpStatusCodeException ex){
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("error", ex.getResponseBodyAsString());
+            return new ResponseEntity<List<Persona>>(new ArrayList<Persona>(),httpHeaders,ex.getStatusCode());
+        }
+
     }
     @GetMapping(value="/personas/{edad1}/{edad2}", produces=MediaType.APPLICATION_JSON_VALUE)
     public List<Persona> buscarEdades(@PathVariable("edad1") int edad1, @PathVariable("edad2") int edad2){
