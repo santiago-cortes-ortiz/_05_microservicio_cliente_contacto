80611a573d541d224fc002819bed691251c9dd27
santiago cortes <jeissoncortes02@outlook.com>
2021-08-06 15:00:16 -0500

    usando autenticacion e interceptor

--- src/main/java/inicio/Application.java
+++ src/main/java/inicio/Application.java
@@ -3,6 +3,7 @@ package inicio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -14,7 +15,11 @@ public class Application {
 
     @Bean
     public RestTemplate template(){
-        return new RestTemplate();
+        BasicAuthenticationInterceptor interceptor;
+        interceptor = new BasicAuthenticationInterceptor("admin","admin");
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add(interceptor);
+        return restTemplate;
     }
 
 }
